216a7218d687fd8c96fb184a5b32e4d5528041b2
EjjiCoUk <88962595+EjjiCoUk@users.noreply.github.com>
2021-08-20 21:43:07 +0100

    Update pkg.js

--- pkg.js
+++ pkg.js
@@ -6,23 +6,44 @@ module.exports.Template = class Template {
     this.prefix = prefix;
     this.token = token;
     this.ownerId = ownerId
-    this.Commands = Object.create(null)
+    this.Commands = new Object
+    this.ActiveCommands = Object.create(null)
+    this.ActiveCommands.test = async (message, args) => {
+      await message.channel.send("hey")
+    }
+    this.ActiveCommands.pong = async (message, args) => {
+      await message.channel.send("ping")
+    }
+    this.ActiveCommands.useCommand = async (message, args) => {
+      this.Commands[args[1]] = true
+      if(!this.ActiveCommands[args[1]]) await message.channel.send(`The Command ${args[1]} Isn't Setup`)
+    }
+    this.ActiveCommands.unUseCommand = async (message, args) => {
+      this.Commands[args[1]] = false
+      if(!this.ActiveCommands[args[1]]) await message.channel.send(`The Command ${args[1]} Isn't Setup`)
+    }
   }
-  addCommand(name) {
+  UseDefault(name) {
     this.Commands[name] = true
+  }
+  UnUseDefault(name) {
+    this.Commands[name] = false
+  }
+  createCommand(name, cmd) {
+    this.ActiveCommands[name] = cmd
   }
   bootup() {
     Client.once('ready', () => {
       console.log("Ready!")
     })
 
-    Client.on('message', message => {
-      args=message.content.split(" ")
-      cmd=args[0].split(this.prefix)[1]
-      if(cmd=="test"){
-        if(!this.Commands.test) return
-        await message.channel.send("hey")
-      }
+    Client.on('message', async (message) => {
+      if(message.author.bot) return;
+      var args=message.content.split(" ")
+      var cmd=args[0].split(this.prefix)[1]
+      if(!this.Commands[cmd]) return;
+      if(!this.ActiveCommands[cmd]) await message.channel.send("This Command hasn't been setup yet")
+      this.ActiveCommands[cmd](message, args)
     })
 
     Client.login(this.token)

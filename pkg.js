ddffe011ccd12f977238570085e46527e7ffbbea
EjjiCoUk <samuel_buckley@outlook.com>
2021-08-20 18:21:34 +0100

    Usable commands

--- pkg.js
+++ pkg.js
@@ -6,11 +6,25 @@ module.exports.Template = class Template {
     this.prefix = prefix;
     this.token = token;
     this.ownerId = ownerId
+    this.Commands = Object.create(null)
+  }
+  addCommand(name) {
+    this.Commands[name] = true
   }
   bootup() {
     Client.once('ready', () => {
       console.log("Ready!")
     })
+
+    Client.on('message', message => {
+      args=message.content.split(" ")
+      cmd=args[0].split(this.prefix)[1]
+      if(cmd=="test"){
+        if(!this.Commands.test) return
+        await message.channel.send("hey")
+      }
+    })
+
     Client.login(this.token)
   }
 }
